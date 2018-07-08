ff3dd5ff2f43c91525d9c7ed54be899d2596b73e
chengyangyang <390518881@qq.com>
2018-07-08 21:34:40 +0800

    a

--- src/main/java/ch/entity/user/UserRealm.java
+++ src/main/java/ch/entity/user/UserRealm.java
@@ -1,13 +1,18 @@
 package ch.entity.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import ch.entity.exception.BusinessException;
@@ -31,16 +36,38 @@ public class UserRealm extends AuthorizingRealm {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-     
         String pwd = user.getPassword();
-        return new SimpleAuthenticationInfo(user, pwd, getName());
+        
+        //加盐处理
+        //ByteSource salt = ByteSource.Util.bytes("Shiro");
+        //new SimpleAuthenticationInfo(user, pwd,salt, this.getName());
+
+        return new SimpleAuthenticationInfo(user, pwd, this.getName());
     }
 
     /**
      * 授权
      */
     @Override
-    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection princ) {
-        return null;
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+    	 // 获取身份信息
+        String username = (String) principals.getPrimaryPrincipal();
+     // 这里使用静态数据模拟
+        List<String> permissions = new ArrayList<String>();
+        permissions.add("user:*");
+        permissions.add("department:*");
+        
+     // 将权限信息封闭为AuthorizationInfo
+        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+        // 模拟数据，添加 manager 角色
+        simpleAuthorizationInfo.addRole("manager");
+        
+        for(String permission:permissions){
+            simpleAuthorizationInfo.addStringPermission(permission);
+        }
+        
+        return simpleAuthorizationInfo;
+        
+
     }
 }
