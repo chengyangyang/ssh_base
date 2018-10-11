7628bc7d4759884f38d9de877fc9471ba1964261
chengeyangyang <390518881@qq.com>
2018-10-11 17:35:07 +0800

    添加文件md5值的计算

--- src/main/java/ch/util/MD5Utils.java
+++ src/main/java/ch/util/MD5Utils.java
@@ -1,7 +1,11 @@
 package ch.util;
 
+import org.apache.commons.codec.binary.Hex;
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.security.MessageDigest;
 
 /**
@@ -62,5 +66,37 @@ public class MD5Utils {
 			return s;
 		}
 	}
+
+	/**
+	 * 获取一个文件的md5值(可处理大文件)
+	 * @return md5 value
+	 */
+	public static String getMD5(File file) {
+		FileInputStream fileInputStream = null;
+		try {
+			MessageDigest MD5 = MessageDigest.getInstance("MD5");
+			fileInputStream = new FileInputStream(file);
+			byte[] buffer = new byte[8192];
+			int length;
+			while ((length = fileInputStream.read(buffer)) != -1) {
+				MD5.update(buffer, 0, length);
+			}
+			return new String(Hex.encodeHex(MD5.digest()));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			try {
+				if (fileInputStream != null){
+					fileInputStream.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+
+
 
 }
