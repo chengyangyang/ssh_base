47977b7844dd7c599112bb80f4a73ce729b4f5a9
chengeyangyang <390518881@qq.com>
2018-12-18 17:40:36 +0800

    添加swagger插件

---
+++ src/main/java/ch/common/swagger/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package ch.common.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Description:
+ *
+ * @author cy
+ * @date 2018年12月18日 14:05
+ * version 1.0
+ */
+@Configuration
+@EnableSwagger2
+@EnableWebMvc
+@ComponentScan(basePackages = {"ch.**"})
+public class SwaggerConfig {
+
+    @Bean
+    public Docket customDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        Contact contact = new Contact("小明", "http://www.cnblogs.com/getupmorning/", "zhaoming0018@126.com");
+        return new ApiInfoBuilder()
+                .title("前台API接口")
+                .description("前台API接口")
+                .contact(contact)
+                .version("1.1.0")
+                .build();
+    }
+
+}
