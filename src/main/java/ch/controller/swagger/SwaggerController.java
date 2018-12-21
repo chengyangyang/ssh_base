be014a78433914e697ca3a99cf5fa6174e71d7e7
chengeyangyang <390518881@qq.com>
2018-12-21 13:56:29 +0800

    swagger 集成spring完成

--- src/main/java/ch/controller/swagger/SwaggerController.java
+++ src/main/java/ch/controller/swagger/SwaggerController.java
@@ -4,9 +4,7 @@ import ch.bean.CommonReponse;
 import ch.bean.Person;
 import io.swagger.annotations.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,16 +16,15 @@ import java.util.List;
  * @date 2018年12月19日 9:06
  * version 1.0
  */
-@Controller
+@RestController
 @RequestMapping("/swagger")
 @Api(tags = "swagger控制层测试",value = "api接口类",description = "测试")
 public class SwaggerController {
 
 
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
-    @ResponseBody
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
     @ApiOperation(value = "测试接口")
-    public CommonReponse listCompound(@ApiParam(value = "用户名称") String userName) {
+    public CommonReponse listCompound(@RequestParam("userName") String userName) {
         List<Person> data = new ArrayList<Person>();
         String msg = data.size() > 0 ? "" : "没有查询到相关记录";
         CommonReponse result = new CommonReponse();
